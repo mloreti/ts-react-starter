b5f62bb689c125974aa7b7a7dd12c88fe613c4cd
Mickey Loreti <mloreti10@gmail.com>
2024-09-06 17:24:34 -0400

    Add biome, remove eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+let root = document.getElementById("root");
+
+if (!root) {
+	root = document.createElement("div");
+	root.id = "root";
+	document.body.appendChild(root);
+}
+
+createRoot(root).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
