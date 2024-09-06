2e3209eaaf94aac8c70099723e62fc221d75563c
Mickey Loreti <mloreti10@gmail.com>
2024-09-06 18:00:03 -0400

    Configure vite env

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-let root = document.getElementById("root");
+const root = document.getElementById("root");
 
-if (!root) {
-	root = document.createElement("div");
-	root.id = "root";
-	document.body.appendChild(root);
-}
-
-createRoot(root).render(
+createRoot(root!).render(
 	<StrictMode>
 		<App />
 	</StrictMode>,
