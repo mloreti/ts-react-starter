5103d17ddebcbc0085e2cefc0629406df167aeae
Mickey Loreti <mloreti10@gmail.com>
2024-09-06 22:28:43 -0400

    Add MSW

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,19 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect, afterEach, beforeAll, afterAll } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { server } from "./mocks/node";
 
 expect.extend(matchers);
 
+beforeAll(() => {
+	server.listen();
+});
+
 afterEach(() => {
-  cleanup();
+	server.resetHandlers();
+	cleanup();
+});
+
+afterAll(() => {
+	server.close();
 });
