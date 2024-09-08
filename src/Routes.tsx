cc9f5c56abcab0eff378e4c30979db4daeb7ea83
Mickey Loreti <mloreti10@gmail.com>
2024-09-08 10:07:04 -0400

    Create pages directory

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import { Route, Switch } from "wouter";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 
 const Routes = () => {
 	return (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
-import { Button } from "../ui/button";
 
 const Home = () => {
 	const [clicked, setClicked] = React.useState(false);
