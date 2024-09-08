abc450412134f585948dfabdad9f64201f4017d2
Mickey Loreti <mloreti10@gmail.com>
2024-09-08 10:26:30 -0400

    Remove underline

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
 	return (
 		<div className="w-screen h-screen content-center">
 			<div className="flex flex-col space-y-4 text-center">
-				<h1 className="text-3xl font-bold underline">Hello world!</h1>
+				<h1 className="text-3xl font-bold">Hello world!</h1>
 				{clicked && <p>You clicked me!</p>}
 				<div>
 					<Button onClick={() => setClicked(!clicked)}>Click me!</Button>
