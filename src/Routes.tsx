561c250f39063d8cbff4b9cb5096e75b3af75474
Mickey Loreti <mloreti10@gmail.com>
2024-09-10 14:44:20 -0400

    Remove transactions

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,11 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
-import Transactions from "./pages/Transactions";
 
 const Routes = () => {
 	return (
 		<div className="w-screen h-screen text-center p-4 flex">
 			<Switch>
 				<Route path="/" component={Home} />
-				<Route path="/transactions" component={Transactions} />
 				<Route>404: No such page!</Route>
 			</Switch>
 		</div>
