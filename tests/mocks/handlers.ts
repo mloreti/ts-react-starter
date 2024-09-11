84d6a2e3942c1d44c2729a4d63d9606afd747ccf
Mickey Loreti <mloreti10@gmail.com>
2024-09-11 11:12:25 -0400

    Lint test files

--- tests/mocks/handlers.ts
+++ tests/mocks/handlers.ts
@@ -1,5 +1,4 @@
-import { http, HttpResponse } from "msw";
-
+// import { http, HttpResponse } from "msw";
 export const handlers = [
 	// http.get("https://api.example.com/user", () => {
 	// 	return HttpResponse.json({
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,6 +1,6 @@
-import { expect, afterEach, beforeAll, afterAll } from "vitest";
-import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/react";
+import { afterAll, afterEach, beforeAll, expect } from "vitest";
 import { server } from "./mocks/node";
 
 expect.extend(matchers);
