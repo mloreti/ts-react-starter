ecf2e2fc4af07fca6b83f0ffcbe675bb757690bd
Mickey Loreti <mloreti10@gmail.com>
2024-09-11 11:11:57 -0400

    Setup coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
-import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +9,13 @@ export default defineConfig({
 		environment: "jsdom",
 		setupFiles: ["./tests/setup.ts"],
 		globals: true,
+		coverage: {
+			provider: "v8",
+			reportsDirectory: "./tests/coverage",
+			reporter: ["text"],
+			include: ["src/**/*"],
+			exclude: ["node_modules", "tests", "src/components/ui/base/**"],
+		},
 	},
 	resolve: {
 		alias: {
